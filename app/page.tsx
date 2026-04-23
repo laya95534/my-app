2ba4f231b7f245ab3f86d9414beed14fd35d22fd
Laya Reddy <your-github-email@example.com>
2026-04-23 13:24:39 +0530

    add homepage redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">My App</h1>
-    </div>
-  );
+  redirect('/login')
 }
